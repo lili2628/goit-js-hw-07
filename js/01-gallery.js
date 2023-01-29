826573161e30231a8f86448597e6257846ac2138
Liliia Paliichuk <113315152+lili2628@users.noreply.github.com>
2023-01-30 00:58:20 +0100

    added HW 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,63 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const galleryCardEl = createGalleryCard(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryCardEl);
+
+galleryEl.addEventListener('click', onGalleryPictureClick);
+
+
+function createGalleryCard(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>
+        `;
+        })
+        .join('');
+}
+
+function onGalleryPictureClick(event) {
+     event.preventDefault();
+
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+
+    const urlOriginalPicture = event.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+        <img
+            width="1280"
+            src="${urlOriginalPicture}"
+            alt=""
+        />
+    `);
+    
+    instance.show();
+
+    const visible = instance.visible();
+
+    document.addEventListener('keydown', onModalKeypressEsc);
+
+    function onModalKeypressEsc(event){
+        if (visible && event.key === 'Escape') { 
+            instance.close();
+        }
+
+    }
+
+}
+
