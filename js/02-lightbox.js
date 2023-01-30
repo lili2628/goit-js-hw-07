707d2fe92c2b0cf443e02256cef1b3a8cc9967df
Liliia Paliichuk <113315152+lili2628@users.noreply.github.com>
2023-01-31 00:16:05 +0100

    added HW 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,3 +3,31 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+const galleryEl = document.querySelector('.gallery');
+const galleryCardEl = createGalleryCard(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryCardEl);
+
+function createGalleryCard(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `       
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        `;
+        })
+        .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a',
+    {
+        nav: false,
+        close: false,
+        overlayOpacity: 0.8,
+        captionSelector: 'img',
+        captionType: 'attr',
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250
+    });
